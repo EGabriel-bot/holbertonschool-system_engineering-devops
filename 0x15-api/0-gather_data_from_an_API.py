5fcae66d0b77ec951481ba95157fc13e1c16bcc0
Eiden <eidnegabriel07@gmail.com>
2022-09-12 19:12:12 -0400

    fix task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
 """Using a REST API return information about TO-DO list progress"""
-import json
-import requests
-import sys
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
 
-idd = sys.argv[1]
-tasks_done = 0
-completed_tasks = []
-total_tasks = 0
+    idd = sys.argv[1]
+    tasks_done = 0
+    completed_tasks = []
+    total_tasks = 0
 
-"""Get request to fetch users"""
-user = requests.get(
-    'https://jsonplaceholder.typicode.com/users/{}'.format(idd))
+    """Get request to fetch users"""
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(idd))
 
-"""Get request to fetch the todo tasks"""
-response = requests.get(
-    'https://jsonplaceholder.typicode.com/todos/')
+    """Get request to fetch the todo tasks"""
+    response = requests.get(
+        'https://jsonplaceholder.typicode.com/todos/')
 
-"""Converting data from user and todos to a string dictionary"""
-users = json.loads(user.text)
-tasks_list = json.loads(response.text)
+    """Converting data from user and todos to a string dictionary"""
+    users = json.loads(user.text)
+    tasks_list = json.loads(response.text)
 
-"""Iterating through todo list"""
-for tasks in tasks_list:
-    if users['id'] == tasks['userId']:
-        if tasks['completed'] is True:
-            completed_tasks.append(tasks['title'])
-            tasks_done += 1
-        total_tasks += 1
+    """Iterating through todo list"""
+    for tasks in tasks_list:
+        if users['id'] == tasks['userId']:
+            if tasks['completed'] is True:
+                completed_tasks.append(tasks['title'])
+                tasks_done += 1
+            total_tasks += 1
 
-print('Employee {} is done with tasks ({}/{}:)'
-      .format(users['name'], tasks_done, total_tasks))
+    print('Employee {} is done with tasks ({}/{}:)'
+          .format(users['name'], tasks_done, total_tasks))
 
-for tasks in completed_tasks:
-    print('     {}'.format(tasks))
+    for tasks in completed_tasks:
+        print('\t {}'.format(tasks))
