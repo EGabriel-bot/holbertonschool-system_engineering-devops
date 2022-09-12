275fffd3ca411d66d60aabb65d39cbb333ac759e
Eiden <eidnegabriel07@gmail.com>
2022-09-12 19:42:33 -0400

    fix task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,20 +10,20 @@ completed_tasks = []
 total_tasks = 0
 
 """Get request to fetch users"""
+user = requests.get(
+    'https://jsonplaceholder.typicode.com/users/{}'.format(int(sys.argv[1])))
+
+"""Get request to fetch the todo tasks"""
+response = requests.get(
+    'https://jsonplaceholder.typicode.com/todos/')
+
+"""Converting data from user and todos to a string dictionary"""
+users = json.loads(user.text)
+tasks_list = json.loads(response.text)
+
+"""Iterating through todo list"""
 if __name__ == "__main__":
     if sys.argv[1].isdigit() is True:
-        user = requests.get(
-            'https://jsonplaceholder.typicode.com/users/{}'.format(int(sys.argv[1])))
-
-        """Get request to fetch the todo tasks"""
-        response = requests.get(
-            'https://jsonplaceholder.typicode.com/todos/')
-
-        """Converting data from user and todos to a string dictionary"""
-        users = json.loads(user.text)
-        tasks_list = json.loads(response.text)
-
-        """Iterating through todo list"""
         for tasks in tasks_list:
             if users['id'] == tasks['userId']:
                 if tasks['completed'] is True:
