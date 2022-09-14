b44596b2d3da67f7c8cabce2f8f1dcdd2db808b0
Eiden <eidnegabriel07@gmail.com>
2022-09-14 18:45:12 -0400

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Using a REST API return information about TO-DO list progress"""
+import csv
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    if sys.argv[1].isdigit() is True:
+        idd = int(sys.argv[1])
+
+        """Get request to fetch users"""
+        user = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(idd))
+        users = json.loads(user.text)
+
+        """Get request to fetch the todo tasks"""
+        response = requests.get(
+            'https://jsonplaceholder.typicode.com/todos/')
+        tasks_list = json.loads(response.text)
+
+        tasks_done = 0
+        completed_tasks = []
+        total_tasks = 0
+        """Iterating through todo list"""
+        for tasks in tasks_list:
+            if users['id'] == tasks['userId']:
+                total_tasks += 1
+                if tasks['completed'] is True:
+                    completed_tasks.append(tasks['title'])
+                    tasks_done += 1
+
+        print('Employee {} is done with tasks({}/{}):'
+              .format(users['name'], tasks_done, total_tasks))
+
+        for tasks in completed_tasks:
+            print('\t {}'.format(tasks))
+
+        with open('{}.json'.format(idd), 'w', newline='') as jsonfile:
+            """generating json file"""
+            forma = []
+            j_myson = {}
+            for tasks in tasks_list:
+                if users['id'] == tasks['userId']:
+                    j_myson["task"] = tasks['title']
+                    j_myson["completed"] = tasks['completed']
+                    j_myson["username"] = user['username']
+                    copy = j_myson.copy()
+                    forma.append(copy)
+                u_info = {user['id']: forma}
+                json.dump(u_info, jsonfile)
