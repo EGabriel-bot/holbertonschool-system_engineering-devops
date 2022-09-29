0af1e4830055d6a52967e1e9fab16d2b451980a6
Eiden Martinez Davila <eidengabriel07@gmail.com>
2022-09-28 21:15:22 -0400

    TASK 0 FIX

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# Fix settings in PHP
-exec {'PHPSetting':
-  command  => 'sed -i "s/phpp/.php/g" /var/www/html/wp-settings.php',
+# Fix PHP settings
+exec { 'PHPSetting':
+  command  => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
   provider => shell,
 }
